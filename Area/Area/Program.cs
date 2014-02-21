8bad21365fe9ac0e2bd8b8898bad53ddfae4e0cd
RMiller28 <Millerrycki5@gmail.com>
2014-02-21 09:58:54 -0500

    New Change

--- Area/Area/Program.cs
+++ Area/Area/Program.cs
@@ -3,17 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Chapter2Problems
+namespace Area
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int length = 15, width = 25;
-            int area = length * width;
-
-            Console.WriteLine("The floor space is {0} square feet.", area);
-
         }
     }
 }
