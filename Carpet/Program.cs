1d5b32b6a6defda453e01d01d5a4a345e9630ac7
RMiller28 <Millerrycki5@gmail.com>
2014-02-21 10:07:38 -0500

    Revert "New Change"
    This reverts commit 8bad21365fe9ac0e2bd8b8898bad53ddfae4e0cd.

--- Carpet/Program.cs
+++ Carpet/Program.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Chapter2Problems
+namespace Carpet
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int length = 15, width = 25;
+            int length = 11, width = 20;
             int area = length * width;
+            double price = 3.27;
+            double cost = price * area;
 
-            Console.WriteLine("The floor space is {0} square feet.", area);
+            Console.WriteLine("The carpet will cost {0}", cost.ToString("C"));
 
         }
     }
