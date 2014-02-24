6d1cf846e57fe7e1cefeb4dc7895c941d2e9a99f
RMiller28 <Millerrycki5@gmail.com>
2014-02-24 10:09:52 -0500

    New Change

--- Yards/Program.cs
+++ Yards/Program.cs
@@ -12,11 +12,7 @@ namespace Hours
             int minutes = 180;
             int hours = minutes / 60;
             minutes = minutes % 60;
-
-
-
-
-
+            
             Console.WriteLine(" The time you worked was {0} Hours {1} minutes", hours, minutes);
             
         }
