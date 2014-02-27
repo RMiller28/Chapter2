1c9756ee522682ca63432653f0a8a96629b1d578
RMiller28 <Millerrycki5@gmail.com>
2014-02-27 10:02:00 -0500

    New Change

---
+++ Area/Eggs.cs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eggs2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int eggs;
+            int chick1 = 6;
+            int chick2 = 4;
+            int chick3 = 6;
+            int chick4 = 8;
+
+            eggs = chick1 + chick2 + chick3 + chick4;
+
+            double dozen = eggs / 12;
+            double total = eggs % dozen;
+
+
+
+            Console.Write(" The total is {0} dozen and {1} eggs", dozen, total);
+
+
+
+
+
+
+
+        }
+    }
+}
--- Payroll/Program.cs
+++ Payroll/Program.cs
@@ -9,6 +9,54 @@ namespace Payroll
     {
         static void Main(string[] args)
         {
+
+            String name, ssn, hpr, hw;
+            double hourspay, hoursworked;
+
+
+
+            double gross, fed, state, moneyperh;
+
+
+
+            Console.Write("Enter your name:");
+            name = Console.ReadLine();
+
+
+            Console.Write("Social Security number:");
+            ssn = Console.ReadLine();
+
+
+            Console.Write("Hourly pay rate:");
+            hpr = Console.ReadLine();
+            hourspay = Convert.ToDouble(hpr);
+
+            Console.Write("Hours worked:");
+            hw = Console.ReadLine();
+            hoursworked = Convert.ToDouble(hw);
+
+            // Do the calculations!!!!!!
+            gross = hourspay * hoursworked;
+            moneyperh = gross / hoursworked;
+            fed = gross * 0.15;
+            state = gross * 0.05;
+
+
+            Console.WriteLine("Payroll Summary for {0}:", name);
+
+            Console.WriteLine("SSN {0}:", ssn);
+
+            Console.WriteLine("You earned {0} at {1} per hour", gross, moneyperh);
+
+
+
+            Console.WriteLine("Gross pay is: {0,10}", gross.ToString("C"));
+
+            Console.WriteLine("Federal withholding is: {0,10}", fed.ToString("C"));
+
+            Console.WriteLine("State withholding is {0,10}", state.ToString("C"));
+
+
         }
     }
 }
